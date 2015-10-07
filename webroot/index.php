3195b2cb571b92eb9f4a2399107f4bac26a41a67
Monika Spielman <monika.spielman@metaltoad.com>
2015-10-06 22:23:09 -0700

    formatting with phpstorm

--- webroot/index.php
+++ webroot/index.php
@@ -1,202 +1,220 @@
 <!doctype html>
 <html class="no-js" lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>All About Arrays</title>
-    <link rel="stylesheet" href="stylesheets/app.css" />
-    <script src="bower_components/modernizr/modernizr.js"></script>
-  </head>
-  <body>
-    <div class="row">
-      <div class="large-12 columns">
-        <h1>All About Arrays</h1>
-      </div>
-    </div>
+<head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>All About Arrays</title>
+  <link rel="stylesheet" href="stylesheets/app.css"/>
+  <script src="bower_components/modernizr/modernizr.js"></script>
+</head>
+<body>
+<div class="row">
+  <div class="large-12 columns">
+    <h1>All About Arrays</h1>
+  </div>
+</div>
 
-    <div class="row">
-      <div class="large-12 columns">
-        <div class="panel">
-          <h3>Not Yahtzee!</h3>
-          <p>Given 5 6-sided dice.</p>
-          <p>
+<div class="row">
+  <div class="large-12 columns">
+    <div class="panel">
+      <h3>Not Yahtzee!</h3>
+
+      <p>Given 5 6-sided dice.</p>
+
+      <p>
+        <?php
+        // Each die could be an index array
+        /*
+				$die1 = array(1,2,3,4,5,6);
+				$die2 = array(1,2,3,4,5,6);
+				$die3 = array(1,2,3,4,5,6);
+				$die4 = array(1,2,3,4,5,6);
+				$die5 = array(1,2,3,4,5,6);
+				*/
+        // An associative array of 5 index arrays for all the dice togather
+        $all_dice = array(
+          'die1' => array( 1, 2, 3, 4, 5, 6 ),
+          'die2' => array( 6, 5, 4, 3, 2, 1 ),
+          'die3' => array( 2, 4, 6, 1, 3, 5 ),
+          'die4' => array( 1, 3, 5, 2, 4, 6 ),
+          'die5' => array( 6, 4, 2, 1, 3, 5 ),
+        );
+        echo 'a var_dump of $all_dice: ';
+        var_dump( $all_dice );
+        ?></p>
+
+      <div class="row">
+        <div class="large-4 medium-4 columns"><p>$roll1_die1 =
+            array_rand($all_dice);<br>
+            echo $roll1_die1;<br>Results in:</p>
           <?php
-          // Each die could be an index array
-          /*
-          $die1 = array(1,2,3,4,5,6);
-          $die2 = array(1,2,3,4,5,6);
-          $die3 = array(1,2,3,4,5,6);
-          $die4 = array(1,2,3,4,5,6);
-          $die5 = array(1,2,3,4,5,6);
-          */
-          // An associative array of 5 index arrays for all the dice togather
-          $all_dice = array(
-            'die1' => array(1,2,3,4,5,6),
-            'die2' => array(6,5,4,3,2,1),
-            'die3' => array(2,4,6,1,3,5),
-            'die4' => array(1,3,5,2,4,6),
-            'die5' => array(6,4,2,1,3,5)
-          );
-          echo 'a var_dump of $all_dice: ';
-          var_dump($all_dice);
-          ?></p>
-          <div class="row">
-            <div class="large-4 medium-4 columns"><p>$roll1_die1 = array_rand($all_dice);<br>
-   echo $roll1_die1;<br>Results in:</p>
-<?php
-   $roll1_die1 = array_rand($all_dice);
-   echo $roll1_die1;
-?>
-            </div>
-            <div class="large-4 medium-4 columns"><p>$roll1_die2 = array_rand($all_dice['die2']);<br>
-   echo $roll1_die2;<br>
-   Results in:
-<?php
-$roll1_die2 = array_rand($all_dice);
-   echo $all_dice[$roll1_die2];
-?>
-            </div>
-            <div class="large-4 medium-4 columns">
-<?php
+          $roll1_die1 = array_rand( $all_dice );
+          echo $roll1_die1;
+          ?>
+        </div>
+        <div class="large-4 medium-4 columns"><p>$roll1_die2 =
+            array_rand($all_dice['die2']);<br>
+            echo $roll1_die2;<br>
+            Results in:
+            <?php
+            $roll1_die2 = array_rand( $all_dice );
+            echo $all_dice[ $roll1_die2 ];
+            ?>
+        </div>
+        <div class="large-4 medium-4 columns">
+          <?php
 
-?>
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-4 medium-4 medium-push-2 columns">
+          ?>
+        </div>
+      </div>
+      <div class="row">
+        <div class="large-4 medium-4 medium-push-2 columns">
 
-            </div>
-            <div class="large-4 medium-4 medium-pull-2 columns">
+        </div>
+        <div class="large-4 medium-4 medium-pull-2 columns">
 
-            </div>
-          </div>
         </div>
       </div>
     </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="large-8 medium-8 columns">
+    <h5>Here&rsquo;s your basic grid:</h5>
+    <!-- Grid Example -->
 
     <div class="row">
-      <div class="large-8 medium-8 columns">
-        <h5>Here&rsquo;s your basic grid:</h5>
-        <!-- Grid Example -->
-
-        <div class="row">
-          <div class="large-12 columns">
-            <div class="callout panel">
-              <p><strong>This is a twelve column section in a row.</strong> Each of these includes a div.panel element so you can see where the columns are - it's not required at all for the grid.</p>
-            </div>
-          </div>
+      <div class="large-12 columns">
+        <div class="callout panel">
+          <p><strong>This is a twelve column section in a row.</strong> Each of
+            these includes a div.panel element so you can see where the columns
+            are - it's not required at all for the grid.</p>
         </div>
-        <div class="row">
-          <div class="large-6 medium-6 columns">
-            <div class="callout panel">
-              <p>Six columns</p>
-            </div>
-          </div>
-          <div class="large-6 medium-6 columns">
-            <div class="callout panel">
-              <p>Six columns</p>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="large-4 medium-4 small-4 columns">
-            <div class="callout panel">
-              <p>Four columns</p>
-            </div>
-          </div>
-          <div class="large-4 medium-4 small-4 columns">
-            <div class="callout panel">
-              <p>Four columns</p>
-            </div>
-          </div>
-          <div class="large-4 medium-4 small-4 columns">
-            <div class="callout panel">
-              <p>Four columns</p>
-            </div>
-          </div>
-        </div>
-
-        <hr />
-
-        <h5>We bet you&rsquo;ll need a form somewhere:</h5>
-        <form>
-          <div class="row">
-            <div class="large-12 columns">
-              <label>Input Label</label>
-              <input type="text" placeholder="large-12.columns" />
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-4 medium-4 columns">
-              <label>Input Label</label>
-              <input type="text" placeholder="large-4.columns" />
-            </div>
-            <div class="large-4 medium-4 columns">
-              <label>Input Label</label>
-              <input type="text" placeholder="large-4.columns" />
-            </div>
-            <div class="large-4 medium-4 columns">
-              <div class="row collapse">
-                <label>Input Label</label>
-                <div class="small-9 columns">
-                  <input type="text" placeholder="small-9.columns" />
-                </div>
-                <div class="small-3 columns">
-                  <span class="postfix">.com</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-12 columns">
-              <label>Select Box</label>
-              <select>
-                <option value="husker">Husker</option>
-                <option value="starbuck">Starbuck</option>
-                <option value="hotdog">Hot Dog</option>
-                <option value="apollo">Apollo</option>
-              </select>
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-6 medium-6 columns">
-              <label>Choose Your Favorite</label>
-              <input type="radio" name="pokemon" value="Red" id="pokemonRed"><label for="pokemonRed">Radio 1</label>
-              <input type="radio" name="pokemon" value="Blue" id="pokemonBlue"><label for="pokemonBlue">Radio 2</label>
-            </div>
-            <div class="large-6 medium-6 columns">
-              <label>Check these out</label>
-              <input id="checkbox1" type="checkbox"><label for="checkbox1">Checkbox 1</label>
-              <input id="checkbox2" type="checkbox"><label for="checkbox2">Checkbox 2</label>
-            </div>
-          </div>
-          <div class="row">
-            <div class="large-12 columns">
-              <label>Textarea Label</label>
-              <textarea placeholder="small-12.columns"></textarea>
-            </div>
-          </div>
-        </form>
       </div>
-
-      <div class="large-4 medium-4 columns">
-        <h5>Try one of these buttons:</h5>
-        <p><a href="#" class="small button">Simple Button</a><br/>
-        <a href="#" class="small radius button">Radius Button</a><br/>
-        <a href="#" class="small round button">Round Button</a><br/>
-        <a href="#" class="medium success button">Success Btn</a><br/>
-        <a href="#" class="medium alert button">Alert Btn</a><br/>
-        <a href="#" class="medium secondary button">Secondary Btn</a></p>
-        <div class="panel">
-          <h5>So many components, girl!</h5>
-          <p>A whole kitchen sink of goodies comes with Foundation. Check out the docs to see them all, along with details on making them your own.</p>
-          <a href="http://foundation.zurb.com/docs/" class="small button">Go to Foundation Docs</a>
+    </div>
+    <div class="row">
+      <div class="large-6 medium-6 columns">
+        <div class="callout panel">
+          <p>Six columns</p>
+        </div>
+      </div>
+      <div class="large-6 medium-6 columns">
+        <div class="callout panel">
+          <p>Six columns</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="large-4 medium-4 small-4 columns">
+        <div class="callout panel">
+          <p>Four columns</p>
+        </div>
+      </div>
+      <div class="large-4 medium-4 small-4 columns">
+        <div class="callout panel">
+          <p>Four columns</p>
+        </div>
+      </div>
+      <div class="large-4 medium-4 small-4 columns">
+        <div class="callout panel">
+          <p>Four columns</p>
         </div>
       </div>
     </div>
 
-    <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="bower_components/foundation/js/foundation.min.js"></script>
-    <script src="js/app.js"></script>
-  </body>
+    <hr/>
+
+    <h5>We bet you&rsquo;ll need a form somewhere:</h5>
+
+    <form>
+      <div class="row">
+        <div class="large-12 columns">
+          <label>Input Label</label>
+          <input type="text" placeholder="large-12.columns"/>
+        </div>
+      </div>
+      <div class="row">
+        <div class="large-4 medium-4 columns">
+          <label>Input Label</label>
+          <input type="text" placeholder="large-4.columns"/>
+        </div>
+        <div class="large-4 medium-4 columns">
+          <label>Input Label</label>
+          <input type="text" placeholder="large-4.columns"/>
+        </div>
+        <div class="large-4 medium-4 columns">
+          <div class="row collapse">
+            <label>Input Label</label>
+
+            <div class="small-9 columns">
+              <input type="text" placeholder="small-9.columns"/>
+            </div>
+            <div class="small-3 columns">
+              <span class="postfix">.com</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="large-12 columns">
+          <label>Select Box</label>
+          <select>
+            <option value="husker">Husker</option>
+            <option value="starbuck">Starbuck</option>
+            <option value="hotdog">Hot Dog</option>
+            <option value="apollo">Apollo</option>
+          </select>
+        </div>
+      </div>
+      <div class="row">
+        <div class="large-6 medium-6 columns">
+          <label>Choose Your Favorite</label>
+          <input type="radio" name="pokemon" value="Red" id="pokemonRed"><label
+            for="pokemonRed">Radio 1</label>
+          <input type="radio" name="pokemon" value="Blue"
+                 id="pokemonBlue"><label for="pokemonBlue">Radio 2</label>
+        </div>
+        <div class="large-6 medium-6 columns">
+          <label>Check these out</label>
+          <input id="checkbox1" type="checkbox"><label for="checkbox1">Checkbox
+            1</label>
+          <input id="checkbox2" type="checkbox"><label for="checkbox2">Checkbox
+            2</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="large-12 columns">
+          <label>Textarea Label</label>
+          <textarea placeholder="small-12.columns"></textarea>
+        </div>
+      </div>
+    </form>
+  </div>
+
+  <div class="large-4 medium-4 columns">
+    <h5>Try one of these buttons:</h5>
+
+    <p><a href="#" class="small button">Simple Button</a><br/>
+      <a href="#" class="small radius button">Radius Button</a><br/>
+      <a href="#" class="small round button">Round Button</a><br/>
+      <a href="#" class="medium success button">Success Btn</a><br/>
+      <a href="#" class="medium alert button">Alert Btn</a><br/>
+      <a href="#" class="medium secondary button">Secondary Btn</a></p>
+
+    <div class="panel">
+      <h5>So many components, girl!</h5>
+
+      <p>A whole kitchen sink of goodies comes with Foundation. Check out the
+        docs to see them all, along with details on making them your own.</p>
+      <a href="http://foundation.zurb.com/docs/" class="small button">Go to
+        Foundation Docs</a>
+    </div>
+  </div>
+</div>
+
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/foundation/js/foundation.min.js"></script>
+<script src="js/app.js"></script>
+</body>
 </html>
