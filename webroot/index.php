d9210103cca2aeebac1cc5b22d8a7ba586499ec0
Monika Spielman <monika.spielman@metaltoad.com>
2015-11-02 14:19:15 -0800

    finally? maybe?

--- webroot/index.php
+++ webroot/index.php
@@ -16,23 +16,22 @@
   $die5 = array(1,2,3,4,5,6);
   */
 // An associative array of 5 index arrays for all the dice togather
-/*$all_dice = array(
+$all_dice = array(
   'die1' => array( 1, 2, 3, 4, 5, 6 ),
   'die2' => array( 6, 5, 4, 3, 2, 1 ),
   'die3' => array( 2, 4, 6, 1, 3, 5 ),
   'die4' => array( 1, 3, 5, 2, 4, 6 ),
   'die5' => array( 6, 4, 2, 1, 3, 5 ),
-);*/
+);
 ?>
 <div>
   <h1>Iteration 1 (In order keys, in order values):</h1>
 <?php
 // Each die is an index array, odds in ascending order, evens in descending order
-$keys = array('green', 'red', 'yellow');
-$values = array('avocado', 'apple', 'banana');
-$all_dice = array_combine($keys, $values);
 
-echo '$all_dice: ( ' . implode(', ', $all_dice) . ' )';
+foreach ($all_dice as $key => $value) {
+  echo $key . '=('. implode(', ',$value) . ')<br>';
+}
 ?>
 </div>
 <div>
