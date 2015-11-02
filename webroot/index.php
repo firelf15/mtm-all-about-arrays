230f46c43db632f3b2123f03b4154b0c56b3e95c
Monika Spielman <monika.spielman@metaltoad.com>
2015-11-01 20:36:02 -0800

    I think I got one

--- webroot/index.php
+++ webroot/index.php
@@ -28,13 +28,20 @@
   <h1>Iteration 1 (In order keys, in order values):</h1>
 <?php
 // Each die is an index array, odds in ascending order, evens in descending order
-//TODO reverse the order of values for the even $i results.
-  for ($i = 1; $i <= 5; $i++) {
+$i = 1;
+$array = array(1, 2, 3, 4, 5, 6);
+foreach ($array as $val) {
+  if (0 == $i % 2) {
+    ${'die' . $i} = array();
+    ${'die' . $i} = range(6,1);// even
+  }
+  else {
     ${'die' . $i} = array();
     ${'die' . $i} = range(1,6);
-    echo '$die' .$i . ' = ('.(implode(', ', ${'die' . $i})). ')<br>';
   }
-
+  echo '$die' .$i . ' = ('.(implode(', ', ${'die' . $i})). ')<br>';
+$i++;
+}
 ?>
 </div>
 <div>
