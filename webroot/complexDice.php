d0b3fd7438fa8310008b746d8e770f7db8ec0ae6
firelf15lin <firelf15lin@firelf.com>
2015-11-15 22:25:10 -0800

    self torture, over achiever?

---
+++ webroot/complexDice.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>All About Arrays</title>
+</head>
+<body>
+<?php
+// Each die could be an index array
+/*
+  $die1 = array(1,2,3,4,5,6);
+  $die2 = array(1,2,3,4,5,6);
+  $die3 = array(1,2,3,4,5,6);
+  $die4 = array(1,2,3,4,5,6);
+  $die5 = array(1,2,3,4,5,6);
+  */
+// An associative array of 5 index arrays for all the dice togather
+global $complex_dice;
+$complex_dice = array(
+  'die1' => array( 1, 2, 3, 4, 5, 6 ),
+  'die2' => array( 6, 5, 4, 3, 2, 1 ),
+  'die3' => array( 2, 4, 6, 1, 3, 5 ),
+  'die4' => array( 1, 3, 5, 2, 4, 6 ),
+  'die5' => array( 6, 4, 2, 1, 3, 5 ),
+);
+// WHAT IF? VARIABLE NUMBR OF SIDES? VARIABLE NUMBER OF DIE? foreach and for with count(array)
+?>
+<div>
+  <a href="extraCredit.php">Extra Credit</a>
+  <h1>Iteration 1 (In order keys, in order values):</h1>
+<?php
+for ( $i = 1; $i < 6; $i++ ) {
+  echo 'die' . $i . ' = ( ';
+  for ( $side = 0; $side <= 5; $side++ ) {
+    echo $complex_dice['die' . $i][$side] . ', ';
+  }
+  echo ' )<br>';
+}
+/* cheater cheater pumpkin eater
+foreach ( $complex_dice as $key => $value ) {
+  echo $key . ' = ( '. implode( ', ', $value ) . ' )<br>';
+}*/
+?>
+</div>
+<div>
+  <h1>Iteration 2 (In order keys, in reverse order values):</h1>
+<?php
+foreach ($complex_dice as $key => $value) {
+  echo $key . ' = ( ';
+    // $comma = '';
+    for ( $i = 5; $i >= 0; $i-- ) {
+      // echo $complex_dice['die1'][2];
+      echo $complex_dice[$key][$i] . ', ';
+    }
+  echo ' )<br>';
+}
+?>
+</div>
+<div>
+  <h1>Iteration 3 (In reverse order keys, in order values):</h1>
+<?php
+foreach (array_reverse($complex_dice) as $key => $value) {
+  echo $key . ' = ( ';
+    // $comma = '';
+    for ( $i = 0; $i < 6; $i++ ) {
+      // echo $complex_dice['die1'][2];
+      echo $complex_dice[$key][$i] . ', ';
+    }
+  echo ' )<br>';
+}
+?>
+</div>
+<div>
+  <h1>Iteration 4 (In reverse order keys, in reverse order values):</h1>
+<?php
+foreach (array_reverse($complex_dice) as $key => $value) {
+  echo $key . ' = ( ';
+    // $comma = '';
+    for ( $i = 5; $i >= 0; $i-- ) {
+      // echo $complex_dice['die1'][2];
+      echo $complex_dice[$key][$i] . ', ';
+    }
+  echo ' )<br>';
+}
+?>
+</div>
+</body>
+</html>
---
+++ webroot/randomDice.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>All About Arrays</title>
+</head>
+<body>
+  <div>
+<h1>Extra Credit</h1>
+<?php
+/*
+Extra Credit:
+1. Build $all_dice array programatically
+2. Associative array should have 5 dice (die1...die5), but the only string used in source code should be "die"
+3. Each die will be an indexed array containing numbers 1-6, in random order, containing no duplicates
+
+ASSOCIATIVE
+foreach ($array as $key => $value) {
+# code...
+}
+OR
+foreach (array_expression as $value) {
+}
+
+INDEX
+for ($i = 1; $i <= 10; $i++) {
+echo $i;
+}
+*/
+$all_dice = array();
+for ( $d = 1; $d <= 5; $d++ ) {
+  if ( $d % 2 !== 0 ) {
+    // it is odd
+    for ( $s = 1; $s <= 6; $s++ ) {
+      $all_dice['die' . $d][] = $s;
+    }
+  } else {
+    // it is even
+    for ( $s = 6; $s >= 0; $s-- ) {
+      $all_dice['die' . $d][] = $s;
+    }
+  }
+}
+echo('<pre>');
+print_r( $all_dice );
+echo('</pre>');
+?>
+</div>
+</body>
+</html>
